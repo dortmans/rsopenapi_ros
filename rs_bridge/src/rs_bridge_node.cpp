7bf03ee8a7c98f7c535b3a3facf863787c0f7d4e
Eric Dortmans <eric.dortmans@gmail.com>
2023-03-16 00:58:20 +0100

    rsopenai build integrated

--- rs_bridge/src/rs_bridge_node.cpp
+++ rs_bridge/src/rs_bridge_node.cpp
@@ -1,10 +1,35 @@
-#include <cstdio>
+#include "Robot.hpp"
+#include "Service.hpp"
 
-int main(int argc, char ** argv)
+#include "rclcpp/rclcpp.hpp"
+
+class RsBridgeNode : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+    RsBridgeNode() : Node("rs_bridge")
+    {
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(200),
+            std::bind(&RsBridgeNode::timerCallback, this));
+            
+            service.start(); 
+    }
 
-  printf("hello world rs_bridge package\n");
-  return 0;
+private:
+    void timerCallback()
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello from RSOPENAPI bridge");
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rsopen::Service service;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<RsBridgeNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
