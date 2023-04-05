41cbbcdf6ab427721cc64a4fc001090f3ea58fa1
Eric Dortmans <eric.dortmans@gmail.com>
2023-04-05 21:34:30 +0200

    Obstacles published and TF improved

--- rs_bridge/launch/rs_bridge_launch.py
+++ rs_bridge/launch/rs_bridge_launch.py
@@ -1,4 +1,4 @@
-from launch import LaunchDescription
+from launch import LaunchDescription, LaunchContext
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
@@ -6,8 +6,10 @@ from launch_ros.actions import Node
 
 from math import pi
 
-
 def generate_launch_description():
+    namespace = ['robot', LaunchConfiguration('robot')]
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')]
     return LaunchDescription([
         DeclareLaunchArgument(
             'robot', default_value='1',
@@ -17,44 +19,15 @@ def generate_launch_description():
             'hash', default_value="'0x7d9066e102eb9a4f'",
             description='Robot hash'
         ),
-        DeclareLaunchArgument(
-            'tf', default_value='true',
-            description='TF broadcast enabled'
-        ),
         Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            arguments = ['--x', '0', '--y', '0', '--z', '0', 
-            						'--yaw', str(-pi/2), '--pitch', '0', '--roll', '0', 
-            						'--frame-id', 'base_link', 
-            						'--child-frame-id', 'base_link_msl']
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            arguments = ['--x', '0', '--y', '0', '--z', '0', 
-            						'--yaw', str(-pi/2), '--pitch', '0', '--roll', '0', 
-            						'--frame-id', 'map', 
-            						'--child-frame-id', 'map_msl']
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            arguments = ['--x', '0', '--y', '0', '--z', '0', 
-            						'--yaw', '0', '--pitch', '0', '--roll', '0', 
-            						'--frame-id', 'map', 
-            						'--child-frame-id', 'odom']
-        ),
-        Node(
+        	#namespace=namespace,
             package='rs_bridge',
             executable='rs_bridge_node',
             name='rs_bridge',
             parameters=[
                 {'robot': LaunchConfiguration('robot')},
-                {'hash': LaunchConfiguration('hash')},
-                {'tf': LaunchConfiguration('tf')}
+                {'hash': LaunchConfiguration('hash')}
             ],
-            output='screen',
-            emulate_tty=True
+            #remappings=remappings
         ),
     ])
--- rs_bridge/src/rs_bridge_node.cpp
+++ rs_bridge/src/rs_bridge_node.cpp
@@ -6,6 +6,7 @@
 #include "rs_bridge_msgs/msg/world_model.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include <geometry_msgs/msg/vector3_stamped.hpp>
 #include "geometry_msgs/msg/transform_stamped.hpp"
@@ -14,6 +15,7 @@
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/static_transform_broadcaster.h"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
@@ -31,7 +33,6 @@ public:
 
         auto robot_id = this->get_parameter("robot").as_int();
         auto hash_str = this->get_parameter("hash").as_string();
-        tf_broadcast_ = this->get_parameter("tf").as_bool();
 
         RCLCPP_INFO(this->get_logger(), "Bridge for robot: %ld with hash: %s",
                     robot_id,
@@ -48,22 +49,49 @@ public:
         // Create proxy to robot
         robot_ = std::make_shared<rsopen::Robot>(robot_id, hash);
 
-        // Create ROS broadcasters, publishers, subscribers, actions
-        odom_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+        // Create ROS timer, publishers, subscribers, actions, broadcasters
+        timer_ = this->create_wall_timer(
+            25ms, std::bind(&RsBridgeNode::timer_callback, this));
+        
         wm_publisher_ = this->create_publisher<rs_bridge_msgs::msg::WorldModel>("worldmodel", 10);
         odom_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
+        obstacles_publisher_ = this->create_publisher<geometry_msgs::msg::PoseArray>("obstacles", 10);
         twist_subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10,
                             std::bind(&RsBridgeNode::twist_callback, this, std::placeholders::_1));
 
-        timer_ = this->create_wall_timer(
-            25ms, std::bind(&RsBridgeNode::timer_callback, this));
-
-        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
-        rclcpp::sleep_for(1s); // to wait for TF to come-up
-
         last_twist_received_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
 
+        tf_static_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+        odom_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
+        // Broadcast static transforms
+        tf2::Quaternion q;
+
+        geometry_msgs::msg::TransformStamped tf_map_ros_to_msl;
+        tf_map_ros_to_msl.header.stamp = this->get_clock()->now();
+        tf_map_ros_to_msl.header.frame_id = "map";
+        tf_map_ros_to_msl.child_frame_id = "map_msl";
+        //q.setRPY(0.0, 0.0, -M_PI_2);
+				//tf_map_ros_to_msl.transform.rotation = tf2::toMsg(q);
+        tf_static_broadcaster_->sendTransform(tf_map_ros_to_msl);
+
+        geometry_msgs::msg::TransformStamped tf_base_link_ros_to_msl;
+        tf_base_link_ros_to_msl.header.stamp = this->get_clock()->now();
+        tf_base_link_ros_to_msl.header.frame_id = "base_link";
+        tf_base_link_ros_to_msl.child_frame_id = "base_link_msl";
+        q.setRPY(0.0, 0.0, -M_PI_2);
+				tf_base_link_ros_to_msl.transform.rotation = tf2::toMsg(q);
+        tf_static_broadcaster_->sendTransform(tf_base_link_ros_to_msl);    
+
+        geometry_msgs::msg::TransformStamped tf_map_to_odom;
+        tf_map_to_odom.header.stamp = this->get_clock()->now();
+        tf_map_to_odom.header.frame_id = "map";
+        tf_map_to_odom.child_frame_id = "odom";
+        tf_static_broadcaster_->sendTransform(tf_map_to_odom);
+
+        rclcpp::sleep_for(1s); // to wait for TF to come-up              
    }
 
 
@@ -85,16 +113,16 @@ private:
 
     template<typename T>
 		bool tf_transform(
-				const T& input_pose,
-				T& transformed_pose,
+				const T& input_stamped,
+				T& output_stamped,
 				const std::string& target_frame)
     {
       try
       {
-        transformed_pose = tf_buffer_->transform(
-      		input_pose, target_frame,
+        output_stamped = tf_buffer_->transform(
+      		input_stamped, target_frame,
       		tf2::durationFromSec(1.0));
-        transformed_pose.header.frame_id = target_frame;
+        output_stamped.header.frame_id = target_frame;
     		return true;
       } catch (const tf2::TransformException & e) {
           RCLCPP_DEBUG(get_logger(), "Failed to transform: (%s)", e.what());
@@ -109,8 +137,7 @@ private:
         double z,
         double rz)
     /*
-     * Position (x, y, z) is relative to world coordinate system
-     * Heading (rz) is relative to robot coordinate system
+     * Position (x, y, z) and angle (rz) are relative to world coordinate system (map_msl)
     */
     {
         geometry_msgs::msg::PoseStamped msl_pose, ros_pose;
@@ -119,7 +146,7 @@ private:
         msl_pose.pose.position.x = x;
         msl_pose.pose.position.y = y;
         msl_pose.pose.position.z = z;
-        double yaw = rz + M_PI_2; // correction for angle between x and y axis
+        double yaw = rz + M_PI_2; // MSL: rz is angle from x-axis. Yaw is angle from y-axis
         tf2::Quaternion q;
         q.setRPY(0.0, 0.0, yaw);
 				msl_pose.pose.orientation = tf2::toMsg(q);
@@ -136,8 +163,7 @@ private:
         double y_vel,
         double z_vel)
     /*
-     * Velocity vector (x_vel, y_vel, z_vel) is relative to MSL world coordinate system (map_msl)
-     * Transform velocity vector to ROS robot coordinate system (base_link)
+     * Velocity vector (x_vel, y_vel, z_vel) is relative to MSL world coordinate system (map_msl).
     */
     {
         geometry_msgs::msg::Vector3Stamped msl_velocity, ros_velocity;
@@ -146,7 +172,7 @@ private:
 				msl_velocity.vector.x = x_vel;
         msl_velocity.vector.y = y_vel;
         msl_velocity.vector.z = z_vel;
-
+        // Transform velocity vector to ROS robot coordinate system (map)
 				tf_transform<geometry_msgs::msg::Vector3Stamped>(msl_velocity, ros_velocity, "base_link");
 
         return ros_velocity;
@@ -158,8 +184,8 @@ private:
         double y_vel,
         double z_vel)
     /*
-     * Velocity vector (x_vel, y_vel, z_vel) is relative to ROS robot coordinate system (base_link)
-     * Transform velocity vector to MSL robot coordinate system (base_link_msl)
+     * Velocity vector (x_vel, y_vel, z_vel) is relative to ROS robot coordinate system (base_link).
+     * Transform velocity vector to MSL robot coordinate system (base_link_msl).
     */
     {
         geometry_msgs::msg::Vector3Stamped msl_velocity, ros_velocity;
@@ -218,40 +244,46 @@ private:
             // Transform pose from ROS map coordinate frame to ROS odom frame
             tf_transform<geometry_msgs::msg::PoseStamped>(ros_pose, ros_odom_pose, "odom");
 
-            if (tf_broadcast_) {
-                // Broadcast odom-->baselink transform
-                geometry_msgs::msg::TransformStamped odom_tf;
-                odom_tf.header.stamp = ros_stamp_from_ts(data_.self.ts);
-                odom_tf.header.frame_id = "odom";
-                odom_tf.child_frame_id = "base_link";
-								odom_tf.transform.translation.x = ros_odom_pose.pose.position.x;
-								odom_tf.transform.translation.y = ros_odom_pose.pose.position.y;
-								odom_tf.transform.translation.z = ros_odom_pose.pose.position.z;
-                odom_tf.transform.rotation = ros_odom_pose.pose.orientation;
+            // Broadcast odom-->base_link transform
+            geometry_msgs::msg::TransformStamped odom_tf;
+            odom_tf.header.stamp = ros_stamp_from_ts(data_.self.ts);
+            odom_tf.header.frame_id = "odom";
+            odom_tf.child_frame_id = "base_link";
+				    odom_tf.transform.translation.x = ros_odom_pose.pose.position.x;
+				    odom_tf.transform.translation.y = ros_odom_pose.pose.position.y;
+				    odom_tf.transform.translation.z = ros_odom_pose.pose.position.z;
+            odom_tf.transform.rotation = ros_odom_pose.pose.orientation;
+            odom_broadcaster_->sendTransform(odom_tf);
 
-                odom_broadcaster_->sendTransform(odom_tf);
-            }
-
-            // Build and publish odometry message
+            // Publish odometry message
             nav_msgs::msg::Odometry odom;
             odom.header.stamp = ros_stamp_from_ts(data_.self.ts);
             odom.header.frame_id = "odom";
 						odom.pose.pose = ros_odom_pose.pose;
             odom.child_frame_id = "base_link";
 						odom.twist.twist.linear = ros_velocity.vector;
-            
             odom_publisher_->publish(odom);
 
-            // Build and publish WorldModel message
+            // Publish obstacles message
+            geometry_msgs::msg::PoseArray obstacles;
+            obstacles.header.frame_id = "map";
+            obstacles.header.stamp = ros_stamp_from_ts(data_.metadata.ts);
+            for (auto & obstacle : data_.obstacles)
+            {
+              obstacles.header.stamp = ros_stamp_from_ts(obstacle.ts);
+              ros_pose = to_ros_pose(obstacle.pose.x, obstacle.pose.y, 0, obstacle.pose.rz);
+              obstacles.poses.push_back(ros_pose.pose);
+            }
+            obstacles_publisher_->publish(obstacles);
+
+            // Publish WorldModel message
             rs_bridge_msgs::msg::WorldModel wm;
             rs_bridge_msgs::msg::Object object;
-
             // Metadata
             wm.header.stamp = ros_stamp_from_ts(data_.metadata.ts);
             wm.metadata.version = data_.metadata.version;
 	          wm.metadata.hash = data_.metadata.hash;
 	          wm.metadata.tick = data_.metadata.tick;
-
             // HwStatus
             wm.hw_status.battery_voltage = data_.hw_status.battery_voltage;
             wm.hw_status.kicker_soc = data_.hw_status.kicker_soc;
@@ -262,7 +294,6 @@ private:
             wm.hw_status.is_battery_low = data_.hw_status.is_battery_low;
             wm.hw_status.is_disk_low = data_.hw_status.is_disk_low;
             wm.hw_status.kinect_num_balls = data_.hw_status.kinect_num_balls;
-
             // PlayerStatus 
             wm.player_status.vendor_id = wm.player_status.vendor_id;
             wm.player_status.shirt_number = data_.player_status.shirt_number;
@@ -274,7 +305,6 @@ private:
             wm.player_status.behavior_state = data_.player_status.behavior_state;
             wm.player_status.control_ball = data_.player_status.control_ball;
             wm.player_status.team_control_ball = data_.player_status.team_control_ball;
-
             // local self
             wm.self.header.stamp = ros_stamp_from_ts(data_.self.ts);
             wm.self.header.frame_id = "map";
@@ -282,7 +312,6 @@ private:
             wm.self.child_frame_id = "base_link";
 						wm.self.twist.twist.linear = ros_velocity.vector;
             wm.self.confidence = data_.self.confidence;
-
             // local ball
             ros_pose = to_ros_pose(data_.ball.pos.x, data_.ball.pos.y, data_.ball.pos.z, 0);
             ros_velocity = to_ros_velocity(data_.ball.vel.x, data_.ball.vel.y, data_.ball.vel.z);
@@ -292,7 +321,6 @@ private:
             wm.ball.child_frame_id = "base_link";
 						wm.ball.twist.twist.linear = ros_velocity.vector;
             wm.ball.confidence = data_.ball.confidence;
-
             // local obstacles 
             for (auto & obstacle : data_.obstacles)
             {
@@ -306,10 +334,7 @@ private:
               object.confidence = obstacle.confidence;
               wm.obstacles.push_back(object);
             }
-
-            // fused
-
-            // fused fball
+            // fused ball
             ros_pose = to_ros_pose(data_.fball.pos.x, data_.fball.pos.y, data_.fball.pos.z, 0);
             ros_velocity = to_ros_velocity(data_.fball.vel.x, data_.fball.vel.y, data_.fball.vel.z);
             wm.fball.header.stamp = ros_stamp_from_ts(data_.fball.ts);
@@ -346,7 +371,6 @@ private:
               object.confidence = one_of_them.confidence;
               wm.them.push_back(object);
             }
-
             // fused selfloc
             ros_pose = to_ros_pose(data_.selfloc.pose.x, data_.selfloc.pose.y, 0, data_.selfloc.pose.rz);
             ros_velocity = to_ros_velocity(data_.selfloc.vel.x, data_.selfloc.vel.y, 0);
@@ -356,8 +380,7 @@ private:
             wm.selfloc.child_frame_id = "base_link";
 						wm.selfloc.twist.twist.linear = ros_velocity.vector;
             wm.selfloc.confidence = data_.selfloc.confidence;
-
-            //wm.selfloc_omni.*
+            // fused selfloc_omni.*
             ros_pose = to_ros_pose(data_.selfloc_omni.pose.x, data_.selfloc_omni.pose.y, 0, data_.selfloc_omni.pose.rz);
             ros_velocity = to_ros_velocity(data_.selfloc_omni.vel.x, data_.selfloc_omni.vel.y, 0);
             wm.selfloc_omni.header.stamp = ros_stamp_from_ts(data_.selfloc_omni.ts);
@@ -366,9 +389,6 @@ private:
             wm.selfloc_omni.child_frame_id = "base_link";
 						wm.selfloc_omni.twist.twist.linear = ros_velocity.vector;
             wm.selfloc_omni.confidence = data_.selfloc_omni.confidence;
-
-            // pass
-
             // pass_detail
             wm.pass_detail.header.stamp = ros_stamp_from_ts(data_.pass_detail.ts);
             wm.pass_detail.valid = data_.pass_detail.valid;
@@ -381,22 +401,17 @@ private:
             wm.pass_detail.origin = ros_pose;
             ros_pose = to_ros_pose(data_.pass_detail.target.x, data_.pass_detail.target.y, 0, 0);
             wm.pass_detail.target = ros_pose;
-
             // pass_request
             wm.pass_request.header.stamp = ros_stamp_from_ts(data_.pass_request.ts);
             wm.pass_request.valid = data_.pass_request.valid;
             wm.pass_request.eta = data_.pass_request.eta;
             ros_pose = to_ros_pose(data_.pass_request.target.x, data_.pass_request.target.y, 0, 0);
             wm.pass_request.target = ros_pose;       
-
-            // planner
-
             // planner ball_pickup
             ros_pose = to_ros_pose(data_.ball_pickup.pos.x, data_.ball_pickup.pos.y, 0, 0);
             wm.ball_pickup.header.stamp = ros_stamp_from_ts(data_.ball_pickup.ts);
             wm.ball_pickup.valid = data_.ball_pickup.valid;
             wm.ball_pickup.pos = ros_pose;
-
             // planner planned_path
             nav_msgs::msg::Path path;
             for (auto & pos : data_.planned_path)
@@ -404,16 +419,12 @@ private:
               ros_pose = to_ros_pose(pos.x, pos.y, 0, 0);
               wm.planned_path.poses.push_back(ros_pose);
             }
-
             // planner time_in_own penalty_area
             wm.time_in_own_penalty_area = rclcpp::Duration::from_seconds(data_.time_in_own_penalty_area);
-
             // planner time_in_opponent_penalty_area
             wm.time_in_opponent_penalty_area = rclcpp::Duration::from_seconds(data_.time_in_opponent_penalty_area);
-
             // Publish the WorldModel message
             wm_publisher_->publish(wm);  
-
         }
         else
         {
@@ -446,17 +457,17 @@ private:
 
     rclcpp::Publisher<rs_bridge_msgs::msg::WorldModel>::SharedPtr wm_publisher_;
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr obstacles_publisher_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_subscriber_;
     rclcpp::TimerBase::SharedPtr timer_;
 
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
     std::shared_ptr<tf2_ros::TransformBroadcaster> odom_broadcaster_;
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
 
     rclcpp::Time last_twist_received_;
- 
-    bool tf_broadcast_;
-   
+  
     std::shared_ptr<rsopen::Service> service_;
     std::shared_ptr<rsopen::Robot> robot_;
     rsopen::interrobot_t data_;   
